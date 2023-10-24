09d56bc54ad0b75ec195f576959d0e285eebb13b
Arpita Bandyopadhyay <66113484+ArpitaBandyopadhyay99@users.noreply.github.com>
2023-10-24 18:50:49 +0530

    Initial Commit

---
+++ src/madlib.py
@@ -0,0 +1,8 @@
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+famous_person = input("Famous Person")
+
+madlib = f"Programming is {adj}!! \n I will {verb} like {famous_person}"
+
+print(madlib)
